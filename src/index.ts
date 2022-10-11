63f9d2e4c74ad5b07442f7d9895243cab6eb1519
peter_alfie <alfie1104@gmail.com>
2022-10-11 14:39:29 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+export const serve = (port: number, filename: string, dir: string) => {
+  const app = express();
+
+  app.listen(port, () => {
+    console.log("Listening on port", port);
+  });
+};
