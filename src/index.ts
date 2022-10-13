3078c8fd69b2991f5e1bd19cdda4c5fa509ebc62
peter_alfie <alfie1104@gmail.com>
2022-10-14 08:49:54 +0900

    Add useProxy parameter to choose which method will be used for loading local-clint

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,26 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  const packagesPath = require.resolve("local-client/build/index.html");
-  app.use(express.static(path.dirname(packagesPath))); //path.dirname(packagesPath) : absolute path of local-client/build/index.html
-  // app.use(
-  //   createProxyMiddleware({
-  //     target: "http://localhost:3000",
-  //     ws: true,
-  //     logLevel: "silent",
-  //   })
-  // );
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: "http://localhost:3000",
+        ws: true,
+        logLevel: "silent",
+      })
+    );
+  } else {
+    const packagesPath = require.resolve("local-client/build/index.html");
+    app.use(express.static(path.dirname(packagesPath))); //path.dirname(packagesPath) : absolute path of local-client/build/index.html
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
