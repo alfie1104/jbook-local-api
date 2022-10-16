0faf22ae5fa1d071e0276bc0ef8130bf9aef6d03
peter_alfie <alfie1104@gmail.com>
2022-10-17 08:47:12 +0900

    Resolving TS catch error and readilg file contents

--- src/routes/cells.ts
+++ src/routes/cells.ts
@@ -8,17 +8,36 @@ interface Cell {
   type: "text" | "code";
 }
 
+interface LocalApiError {
+  code: string;
+}
+
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
 
   const fullPath = path.join(dir, filename);
 
   router.get("/cells", async (req, res) => {
-    //Make sure the cell storage file exists
-    //If it does not exists, add in a default list of cells
-    //Read the file
-    //Parse a list of cells out of it
-    //Send list of cells back to browser
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === "string";
+    };
+
+    try {
+      //Read the file
+      const result = await fs.readFile(fullPath, { encoding: "utf-8" });
+      res.send(JSON.parse(result));
+    } catch (err) {
+      //If read throws an error
+      //Inspect the error, see if it says that the file doesn't exist
+      if (isLocalApiError(err)) {
+        if (err.code === "ENOENT") {
+          await fs.writeFile(fullPath, "[]", "utf-8");
+          res.send([]);
+        }
+      } else {
+        throw err;
+      }
+    }
   });
 
   router.post("/cells", async (req, res) => {
