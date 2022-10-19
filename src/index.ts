0d636e665fbb058e482c0dd891e235e551437b85
peter_alfie <alfie1104@gmail.com>
2022-10-20 08:41:22 +0900

    Refactoring npm setting

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,9 @@ export const serve = (
       })
     );
   } else {
-    const packagesPath = require.resolve("local-client/build/index.html");
+    const packagesPath = require.resolve(
+      "@jsnote-peter/local-client/build/index.html"
+    );
     app.use(express.static(path.dirname(packagesPath))); //path.dirname(packagesPath) : absolute path of local-client/build/index.html
   }
 
